1d1c32b38d017772b0d17c36049d220d2b3eaf55
chris <bolema12@gmail.com>
2017-03-15 15:56:59 +0800

    数据库配置更改

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',# 或者使用 mysql.connector.django
-    'NAME': 'test',
-    'USER': 'root',
-    'PASSWORD': 'root',
-    'HOST':'localhost',
-    'PORT':'3306',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.mysql',# 或者使用 mysql.connector.django
+#     'NAME': 'test',
+#     'USER': 'root',
+#     'PASSWORD': 'root',
+#     'HOST':'localhost',
+#     'PORT':'3306',
+#     }
+# }
 
 
 # Password validation
