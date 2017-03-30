cc17b3e14254171a396f83a0c77e44eb50cc2896
chris <bolema12@gmail.com>
2017-03-30 10:39:27 +0800

    决策树算法

--- deepmind/kNN/kNN2.py
+++ deepmind/kNN/kNN2.py
@@ -5,6 +5,9 @@
 from numpy import *
 import operator
 
+import matplotlib
+import matplotlib.pylab as plt
+
 
 def file2matrix(filename):
     fr = open(filename)
@@ -15,13 +18,39 @@ def file2matrix(filename):
     index = 0
     for line in arrayOLines:
         line = line.strip()
-        listFromLine = line.split('\t')
+        listFromLine = line.split('    ')
         returnMat[index, :] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
+        classLabelVector.append(int(float(listFromLine[-1])))
         index += 1
     return returnMat, classLabelVector
 
 
+def draw():
+    datingDataMat, datgingLabels = file2matrix("data_test/datingtestSet.txt")
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datgingLabels), 15.0 * array(datgingLabels))
+    plt.show()
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataset = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
 if __name__ == '__main__':
     datingDataMat, datgingLabels = file2matrix("data_test/datingtestSet.txt")
-    print()
+    print("datingDataMat\n", datingDataMat)
+    print("datgingLabels\n", datgingLabels)
+    # draw()
+    normDataSet, ranges, minVals = autoNorm(datingDataMat)
+    print("normDataSet\n", normDataSet)
+    print("ranges\n", ranges)
+    print("minVals\n", minVals)
+
